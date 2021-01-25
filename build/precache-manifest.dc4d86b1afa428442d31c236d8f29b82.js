bc5f3c87b62fdfe65c2bd71ac6265990965c6fb3
Mohamed Shafee <mshafee1993@gmail.com>
2021-01-24 22:39:29 -0500

    add _redirects file

---
+++ build/precache-manifest.dc4d86b1afa428442d31c236d8f29b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "660561186dc775335481",
+    "url": "/static/css/main.91ef06ea.chunk.css"
+  },
+  {
+    "revision": "660561186dc775335481",
+    "url": "/static/js/main.66056118.chunk.js"
+  },
+  {
+    "revision": "e99462cfbdd539e145c2",
+    "url": "/static/js/1.e99462cf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db74408484083cb69eccef2e06f7cac2",
+    "url": "/index.html"
+  }
+];
